2400a69dcc16afbdf5d4ea296cffd556d21c672f
Amaury Couturier <amo.couturier@gmail.com>
2023-10-23 17:32:24 -0400

    Random

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float respawnThreshold = -6.0f;
+
+    private Vector3 respawnPoint;
+
+    void Start()
+    {
+        respawnPoint = player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.y <= respawnThreshold)
+        {
+            player.position = respawnPoint;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Checkpoint")
+        {
+            Debug.Log("Checkpoint");
+            respawnPoint = player.position;
+        }
+    }
+}
